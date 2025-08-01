a012ceb7ecc727bac076b8ce22505a96e22784a0
Notkenftr <justkenftr@gmail.com>
2025-08-01 22:29:19 +0700

    update message p2

--- cogs/blackList.py
+++ cogs/blackList.py
@@ -38,9 +38,9 @@ class blackListCommandGroup(app_commands.Group):
             password = password.hexdigest()
             if password == hashPassword:
                 data['blackList'].append(serverid)
-                await interaction.followup.send(f"added {serverid} to the blacklist")
+                await interaction.followup.send(f"``✅``  added {serverid} to the blacklist")
             else:
-                await interaction.followup.send(f"The password is incorrect")
+                await interaction.followup.send(f"``❌``  The password is incorrect")
         with open(gatePath, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=4)
     @app_commands.command(name='remove', description='remove server id from the blacklist')
@@ -53,27 +53,27 @@ class blackListCommandGroup(app_commands.Group):
         baseDir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         gatePath = os.path.join(baseDir, 'database', f'{interaction.channel.id}.json')
         if not os.path.exists(gatePath):
-            await interaction.followup.send(f"This channel does not have a gate yet.")
+            await interaction.followup.send(f"``❌`` This channel does not have a gate yet.")
             return
         with open(gatePath, 'r', encoding='utf-8') as f:
             data =  json.load(f)
         hashPassword = data['password']
         if not serverid.isdigit():
-            await interaction.followup.send('serverid can only contain numbers')
+            await interaction.followup.send('``❌`` serverid can only contain numbers')
             return
         if not password:
             if interaction.user.id == data['ownerId'] or interaction.user.guild_permissions.administrator:
                 data['blackList'].remove(serverid)
-                await interaction.followup.send(f'removed {serverid} from the blacklist')
+                await interaction.followup.send(f'``✅`` removed {serverid} from the blacklist')
             else:
-                await interaction.followup.send(f"You need to enter the password.")
+                await interaction.followup.send(f"``❌`` You need to enter the password.")
         else:
             password = hashlib.sha512(password.encode())
             password = password.hexdigest()
             if password == hashPassword:
                 data['blackList'].append(serverid)
-                await interaction.followup.send(f"removed {serverid} from the blacklist")
+                await interaction.followup.send(f"``✅`` removed {serverid} from the blacklist")
             else:
-                await interaction.followup.send(f"The password is incorrect")
+                await interaction.followup.send(f"``❌`` The password is incorrect")
         with open(gatePath, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=4)
--- cogs/connect.py
+++ cogs/connect.py
@@ -13,13 +13,13 @@ class connect(commands.Cog):
     async def connect(self,interaction: discord.Interaction,gateid: str, password: str =None):
         await interaction.response.defer(thinking=True)
         if not interaction.user.guild_permissions.administrator:
-            await interaction.followup.send(f"You do not have enough permissions to connect.")
+            await interaction.followup.send(f"``❌`` You do not have enough permissions to connect.")
             return
         base = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         localPath = os.path.join(base, "database", f'{interaction.channel.id}.json')
         gatePath = os.path.join(base, "database", f'{gateid}.json')
         if not os.path.exists(gatePath):
-            await interaction.followup.send(f"the gate does not exist")
+            await interaction.followup.send(f"``❌`` the gate does not exist")
             return
         with open(gatePath, 'r', encoding='utf-8') as f:
             data = json.load(f)
@@ -28,26 +28,26 @@ class connect(commands.Cog):
         if not password:
             if interaction.user.id == ownerId:
                 if interaction.guild.id in data['blackList']:
-                    await interaction.followup.send(f"You are on the blacklist.")
+                    await interaction.followup.send(f"``❌`` You are on the blacklist.")
                     return
                 if interaction.channel.id in data['linkList']:
-                    await interaction.followup.send(f"you are already connected")
+                    await interaction.followup.send(f"``❌`` you are already connected")
                     return
                 data['linkList'].append(interaction.channel.id)
-                await interaction.followup.send(f"successful connection")
+                await interaction.followup.send(f"``✅`` successful connection")
             else:
-                await interaction.followup.send(f"You need to enter the password.")
+                await interaction.followup.send(f"``❌`` You need to enter the password.")
         else:
             password = hashlib.sha512(password.encode())
             password = password.hexdigest()
             if password == hashPassword:
                 if interaction.guild.id in data['blackList']:
-                    await interaction.followup.send(f"You are on the blacklist.")
+                    await interaction.followup.send(f"``❌`` You are on the blacklist.")
                     return
                 if interaction.channel.id in data['linkList']:
-                    await interaction.followup.send(f"you are already connected")
+                    await interaction.followup.send(f"``❌`` you are already connected")
                     return
                 data['linkList'].append(interaction.channel.id)
-                await interaction.followup.send(f"successful connection")
+                await interaction.followup.send(f"``✅`` successful connection")
         with open(gatePath, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=4)
