3ccdac26a1a6f2c8175894e2437b00a96df1e0c2
Notkenftr <justkenftr@gmail.com>
2025-08-01 22:27:50 +0700

    update message

--- cogs/blackList.py
+++ cogs/blackList.py
@@ -14,25 +14,25 @@ class blackListCommandGroup(app_commands.Group):
     async def blackList(self, interaction: discord.Interaction, serverid: str, password:str = None):
         await interaction.response.defer(thinking=True)
         if not interaction.user.guild_permissions.administrator:
-            await interaction.followup.send(f"You do not have enough permissions to connect.")
+            await interaction.followup.send(f"``❌`` You do not have enough permissions to connect.")
             return
         baseDir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         gatePath = os.path.join(baseDir, 'database', f'{interaction.channel.id}.json')
         if not os.path.exists(gatePath):
-            await interaction.followup.send(f"This channel does not have a gate yet.")
+            await interaction.followup.send(f"``❌`` This channel does not have a gate yet.")
             return
         with open(gatePath, 'r', encoding='utf-8') as f:
             data =  json.load(f)
         hashPassword = data['password']
         if not serverid.isdigit():
-            await interaction.followup.send('serverid can only contain numbers')
+            await interaction.followup.send('``❌`` serverid can only contain numbers')
             return
         if not password:
             if interaction.user.id == data['ownerId']:
                 data['blackList'].append(serverid)
-                await interaction.followup.send(f"added {serverid} to the blacklist")
+                await interaction.followup.send(f"``✅`` added {serverid} to the blacklist")
             else:
-                await interaction.followup.send(f"You need to enter the password.")
+                await interaction.followup.send(f"``❌`` You need to enter the password.")
         else:
             password = hashlib.sha512(password.encode())
             password = password.hexdigest()
@@ -48,7 +48,7 @@ class blackListCommandGroup(app_commands.Group):
     async def remove(self,interaction: discord.Interaction, serverid: str, password:str =  None):
         await interaction.response.defer(thinking=True)
         if not interaction.user.guild_permissions.administrator:
-            await interaction.followup.send(f"You do not have enough permissions to connect.")
+            await interaction.followup.send(f"``❌`` You do not have enough permissions to connect.")
             return
         baseDir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         gatePath = os.path.join(baseDir, 'database', f'{interaction.channel.id}.json')
